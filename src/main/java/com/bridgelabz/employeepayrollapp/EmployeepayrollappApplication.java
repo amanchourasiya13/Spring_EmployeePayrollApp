1dd78381a67619bcd2c63db8ae15b5055f7c7ce4
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-02-25 12:35:13 +0530

    [Aman] Add: Set application.properties to connect with sql.

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollappApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollappApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeepayrollappApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(EmployeepayrollappApplication.class, args);
 	}
 
