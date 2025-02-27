1cad2c207224b636ed02109581a02271dfe9d690
aman chourasiya <157024772+amanchourasiya13@users.noreply.github.com>
2025-02-27 10:25:57 +0530

    [Aman] Using Lombok For Logging.

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollappApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollappApplication.java
@@ -1,11 +1,11 @@
 package com.bridgelabz.employeepayrollapp;
 
-<<<<<<< HEAD
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-=======
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,16 +13,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
->>>>>>> UC7
+
 public class EmployeepayrollappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeepayrollappApplication.class, args);
-<<<<<<< HEAD
+
     }
 
-=======
+
         log.info("Employee Payroll App Started");
     }
->>>>>>> UC7
+
 }
