7622c31e158c213531c95e3e0e70bb1483d3d49a
aman chourasiya <157024772+amanchourasiya13@users.noreply.github.com>
2025-03-07 15:01:03 +0530

    [Aman] Exception Handler.

--- src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -1,24 +1,3 @@
-//package com.bridgelabz.employeepayrollapp.exceptions;
-//
-//import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.MethodArgumentNotValidException;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//
-//@ControllerAdvice
-//public class EmployeePayrollExceptionHandler {
-//
-//    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-//        @ExceptionHandler(EmployeePayrollException.class)
-//        public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception){
-//
-//            ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", exception.getMessage());
-//            return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
-//        }
-//    }
-//}
 package com.bridgelabz.employeepayrollapp.exceptions;
 
 import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
