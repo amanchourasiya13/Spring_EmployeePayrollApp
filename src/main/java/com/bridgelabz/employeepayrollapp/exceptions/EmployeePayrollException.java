b08dde24d6c1679b5db4fb4c934963a35cc1e948
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-03-07 15:15:49 +0530

    [Aman] main file to run program.

--- src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollException.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.employeepayrollapp.exceptions;
-
-public class EmployeePayrollException extends  RuntimeException{
-    public  EmployeePayrollException(String message){
-        super(message);
-    }
-}
